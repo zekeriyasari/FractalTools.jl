b71d4b895206dc65b1103f2bbdacc3046e9271a3
Zekeriya <zekeriya.sari.deu@gmail.com>
2021-02-01 20:13:38 +0300

    Update docs...

--- docs/make.jl
+++ docs/make.jl
@@ -7,20 +7,20 @@ makedocs(;
     format=Documenter.HTML(),
     pages=[
         "API" => "index.md",
-        "Notes" => [
-            "Interpolation" => [
-                "1D Fractal Interpolation" => "manual/interpolation/one_dimensional_interpolation.md",
-                "2D Fractal Interpolation" => "manual/interpolation/two_dimensional_interpolation.md",
-                "1D Hidden Fractal Interpolation" => "manual/interpolation/one_dimensional_hidden_interpolation.md",
-                "2D Hidden Fractal Interpolation" => "manual/interpolation/two_dimensional_hidden_interpolation.md",
-                ],
-            "Integration" => [
-                "1D Fractal Integration" => "manual/integration/one_dimensional_integration.md",
-                "2D Fractal Integration" => "manual/integration/two_dimensional_integration.md",
-                "1D Hidden Fractal Integration" => "manual/integration/one_dimensional_hidden_integration.md",
-                "2D Hidden Fractal Integration" => "manual/integration/two_dimensional_hidden_integration.md",
-                ],
-        ],
+        # "Notes" => [
+        #     "Interpolation" => [
+        #         "1D Fractal Interpolation" => "manual/interpolation/one_dimensional_interpolation.md",
+        #         "2D Fractal Interpolation" => "manual/interpolation/two_dimensional_interpolation.md",
+        #         "1D Hidden Fractal Interpolation" => "manual/interpolation/one_dimensional_hidden_interpolation.md",
+        #         "2D Hidden Fractal Interpolation" => "manual/interpolation/two_dimensional_hidden_interpolation.md",
+        #         ],
+        #     "Integration" => [
+        #         "1D Fractal Integration" => "manual/integration/one_dimensional_integration.md",
+        #         "2D Fractal Integration" => "manual/integration/two_dimensional_integration.md",
+        #         "1D Hidden Fractal Integration" => "manual/integration/one_dimensional_hidden_integration.md",
+        #         "2D Hidden Fractal Integration" => "manual/integration/two_dimensional_hidden_integration.md",
+        #         ],
+        # ],
     ],
     repo="https://github.com/zekeriyasari/FractalTools.jl/blob/{commit}{path}#L{line}",
     sitename="FractalTools.jl",
